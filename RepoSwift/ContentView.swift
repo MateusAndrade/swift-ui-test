3aea0ae1f174172afec8045c7d5eb19e089aae02
MateusAndrade <mateus.andrade47@outlook.com>
2019-12-27 10:19:24 -0300

    Initial Commit

---
+++ RepoSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RepoSwift
+//
+//  Created by Mateus Neri on 27/12/19.
+//  Copyright © 2019 Mateus Neri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
